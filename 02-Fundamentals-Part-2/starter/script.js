6ea8f23c8699e2112ee65c286da1902b087d6811
Markus <markus@droptableuser.me>
2021-12-30 22:43:25 +0100

    day3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -286,7 +286,7 @@ const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
 function calcTip(bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 function calcAverage(arr) {
   let sum = 0;
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,55 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+/*
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    //value: Number(prompt('Degrees Celsius:')),
+    value: 10,
+  };
 
+  console.table(measurement);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
+*/
+const Test = {
+  Data1: [17, 21, 23],
+  Data2: [12, 5, -5, 0, 4],
+};
+
+function printForecast(arr) {
+  let finalString = '';
+  arr.forEach((v, i) => {
+    finalString += `... ${v}°C in ${i + 1} days `;
+  });
+  return finalString + '...';
+}
+
+console.log(printForecast(Test.Data1));
+
+console.log(printForecast(Test.Data2));
